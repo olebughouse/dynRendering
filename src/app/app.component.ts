5f5e480cd8b144926812e4f81ec9a61688a96ac4
Oliver Backhaus <olli.backhaus@googlemail.com>
2017-11-25 13:01:21 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, HostListener, HostBinding, ElementRef, Renderer} from '@angular/core';
+import {ChildComponent} from "./child/child.component";
+import {AnotherChildComponent} from "./another-child/another-child.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  title = 'app works!';
+  childComponentHolder: any;
+  wasClicked: boolean = false;
+
+  @ViewChild('parent', {read: ViewContainerRef})
+  parent: ViewContainerRef;
+ 
+  constructor (private componentFactoryResolver: ComponentFactoryResolver, private el: ElementRef, private renderer: Renderer) {
+    const childComponent = this.componentFactoryResolver.resolveComponentFactory(ChildComponent); 
+    console.log("childComponent" + childComponent);
+    console.log("parent");
+    const anotherChildComponent = this.componentFactoryResolver.resolveComponentFactory(AnotherChildComponent)
+    let anotherChildComponentHolder;
+    
+    
+    setTimeout(()=>{
+      // at this point we want the "child" component to be rendered into the app.component:
+      //this.childComponentHolder = this.parent.createComponent(childComponent);
+      this.parent.createComponent(childComponent);
+      setTimeout(()=>{
+      // at this point we want the "another-child" component to be rendered into the app.component:
+      anotherChildComponentHolder = this.parent.createComponent(anotherChildComponent);
+      setTimeout(()=> anotherChildComponentHolder.destroy(), 500)
+      }, 1000);
+    }, 1000);
+  }
+
+  removeChild(): void {
+    //this.childComponentHolder.destroy();    
+  }
+  
+  @HostBinding('style.color') color: string;
+
+  @HostListener('click') 
+  onClick(): void {
+    if(!this.wasClicked) {
+      this.color = "green";
+      this.wasClicked = true;
+      console.log("none");
+      this.renderer.setElementStyle(this.el.nativeElement.children[0], 'visibility', 'hidden');
+    }
+    else {
+      this.color = "black";
+      this.wasClicked = false; 
+      console.log("block");
+      this.renderer.setElementStyle(this.el.nativeElement.children[0], 'visibility', 'visible');
+    }
+    console.log("click fired");
+    window.alert("color changed to green");
+    console.log("first Child: " + this.el.nativeElement.children[0].innerHTML);
+    console.log("number of children: " + this.el.nativeElement.children.length);
+    for(let i=0; i < this.el.nativeElement.children.length; i++) {
+      console.log("child no. " + i + " : " + this.el.nativeElement.children[i].innerHTML);
+    }
+
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { AnotherChildComponent } from './another-child/another-child.component';
+import {ImageZoom} from './image-zoom.directive';
+import {ImageZoomLens} from './image-zoom-lens.component';
+import {ImageZoomContainer} from './image-zoom-container.component';
+
+import { HiddenDirective } from './hidden.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    AnotherChildComponent,
+    ImageZoom, ImageZoomContainer, ImageZoomLens,
+    HiddenDirective 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
+  exports: [HiddenDirective, ImageZoom],
   providers: [],
+  entryComponents: [AppComponent, ChildComponent, AnotherChildComponent,
+    ImageZoomContainer, ImageZoomLens],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
